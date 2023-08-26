20efebf71664be21970fe05c5fcd049abf3e9012
l1qwy <liqwy@yandex.ru>
2023-08-26 04:31:39 +0300

    use helmet in app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  helmet({
+    extended: true,
+  }),
+);
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
